58c1ea2fca9e68bbb9afaf6595fafb9c15a6a0d5
AlexFaernon <alexpavlenko6666@rambler.ru>
2021-04-23 22:24:46 +0500

    platform placer

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -11,37 +11,62 @@ public class Player : MonoBehaviour
     [SerializeField] private KeyCode keyRight = KeyCode.D;
     [SerializeField] private Vector3 moveDirection = Vector3.forward;
     [SerializeField] private Vector3 moveSide = Vector3.right;
-    public bool[,] map = {{true, true}, {true, true}};
+
+    public Map Map = new Map(new[,]
+        {{true, true, true, true}, {true, true, true, true}, {true, true, true, true}, {true, true, true, true}});
+    public GameObject Platform;
+
+    private void Start()
+    {
+        for (var x = 0; x < Map.map.GetLength(0); x++)
+        {
+            for (var z = 0; z < Map.map.GetLength(0); z++)
+            {
+                Instantiate(Platform, new Vector3(x, 0, z), new Quaternion());
+            }
+        }
+        
+        GetComponent<Transform>().position = new Vector3(0,0.2f,0);
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(keyForward))
         {
-            if (Check(GetComponent<Transform>().position + moveDirection))
+            if (Map.Check(GetComponent<Transform>().position + moveDirection))
                 GetComponent<Transform>().position += moveDirection;
         }
         if (Input.GetKeyDown(keyBackward))
         {
-            if (Check(GetComponent<Transform>().position - moveDirection))
+            if (Map.Check(GetComponent<Transform>().position - moveDirection))
                 GetComponent<Transform>().position -= moveDirection;
         }
         if (Input.GetKeyDown(keyRight))
         {
-            if (Check(GetComponent<Transform>().position + moveSide))
+            if (Map.Check(GetComponent<Transform>().position + moveSide))
                 GetComponent<Transform>().position += moveSide;
         }
         if (Input.GetKeyDown(keyLeft))
         {
-            if (Check(GetComponent<Transform>().position - moveSide))
+            if (Map.Check(GetComponent<Transform>().position - moveSide))
                 GetComponent<Transform>().position -= moveSide;
         }
     }
-    
+}
+
+public class Map
+{
+    public readonly bool[,] map;
+
+    public Map(bool[,] map)
+    {
+        this.map = map;
+    }
+
     public bool Check(Vector3 vector)
     {
-        // return vector.x >= 0 && vector.x < map.GetLength(0) &&
-        //        vector.z >= 0 && vector.z < map.GetLength(1) &&
-        //        map[(int) vector.x, (int) vector.z];
-        return true;
+        return vector.x >= 0 && vector.x < map.GetLength(0) &&
+               vector.z >= 0 && vector.z < map.GetLength(1) &&
+               map[(int) vector.x, (int) vector.z];
     }
 }
